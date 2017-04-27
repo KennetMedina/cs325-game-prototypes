c806871557df83a2a5ffe876b2b0437f589c8c9b
KennetMedina <kmedina2@gmu.edu>
2017-04-26 23:06:49 -0400

    books test

--- Iterative Sequence 2 - digital prototype 1/js/Game.js
+++ Iterative Sequence 2 - digital prototype 1/js/Game.js
@@ -117,25 +117,33 @@ BasicGame.Game.prototype = {
 
         this.game.camera.follow(this.pbody);
 
+        //this.books = this.game.add.group();
+
+
         this.book1 = this.game.add.sprite(48, 48, 'book');
         this.book1.animations.add('pturn1', [0, 1, 2, 3, 4, 5, 6, 7, 8], 10, true);
         this.book1.scale.setTo(0.25, 0.25);
+        this.game.physics.enable(this.book1, Phaser.Physics.ARCADE);
         this.book1.animations.play('pturn1');
         this.book2 = this.game.add.sprite(264, 312, 'book');
         this.book2.animations.add('pturn2', [0, 1, 2, 3, 4, 5, 6, 7, 8], 10, true);
         this.book2.scale.setTo(0.25, 0.25);
+        this.game.physics.enable(this.book2, Phaser.Physics.ARCADE);
         this.book2.animations.play('pturn2');
         this.book3 = this.game.add.sprite(768, 120, 'book');
         this.book3.animations.add('pturn3', [0, 1, 2, 3, 4, 5, 6, 7, 8], 10, true);
         this.book3.scale.setTo(0.25, 0.25);
+        this.game.physics.enable(this.book3, Phaser.Physics.ARCADE);
         this.book3.animations.play('pturn3');
         this.book4 = this.game.add.sprite(96, 395, 'book');
         this.book4.animations.add('pturn4', [0, 1, 2, 3, 4, 5, 6, 7, 8], 10, true);
         this.book4.scale.setTo(0.25, 0.25);
+        this.game.physics.enable(this.book4, Phaser.Physics.ARCADE);
         this.book4.animations.play('pturn4');
         this.book5 = this.game.add.sprite(456, 668, 'book');
         this.book5.animations.add('pturn5', [0, 1, 2, 3, 4, 5, 6, 7, 8], 10, true);
         this.book5.scale.setTo(0.25, 0.25);
+        this.game.physics.enable(this.book5, Phaser.Physics.ARCADE);
         this.book5.animations.play('pturn5');
 
         this.pfeet.bringToTop();
