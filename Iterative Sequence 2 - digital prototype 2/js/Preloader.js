1441c3ea9c95800e8b8ae6b5bb868a2b31cd86be
KennetMedina <kmedina2@gmu.edu>
2017-05-04 02:08:16 -0400

    test11

--- Iterative Sequence 2 - digital prototype 2/js/Preloader.js
+++ Iterative Sequence 2 - digital prototype 2/js/Preloader.js
@@ -37,8 +37,8 @@ BasicGame.Preloader.prototype = {
 		//this.load.spritesheet('body', 'assets/body.png');
 	    this.load.tilemap('maze', 'assets/maze.json', null, Phaser.Tilemap.TILED_JSON);
 	    this.load.image('tiles-2', 'assets/tiles-2.png');
-	    this.load.image('walls', 'assets/walls.png');
-	    this.load.image('floor', 'assets/floor.png');
+	    this.load.image('walls', 'assets/walls2.png');
+	    this.load.image('floor', 'assets/floor3.png');
 
 	},
 
