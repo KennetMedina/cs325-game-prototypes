9aebb26aadb41f3c3a737145d4df2dd975e5c6c2
KennetMedina <kmedina2@gmu.edu>
2017-05-04 03:24:11 -0400

    test

--- Iterative Sequence 2 - digital prototype 2/js/Game.js
+++ Iterative Sequence 2 - digital prototype 2/js/Game.js
@@ -239,11 +239,11 @@ BasicGame.Game.prototype = {
                 this.isIdle = true;
         }
         if (Math.abs(speedX) + Math.abs(speedY) < 4 && Math.abs(speedX) + Math.abs(speedY) > 0) {
-            var color = this.wallsBmd.getPixel32(this.pfeet.x + speedX + this.pfeet.width / 2, this.pfeet.y + speedY + this.pfeet.height / 2);
+            var color = this.wallsBmd.getPixel32(Math.round(this.pfeet.x + speedX + this.pfeet.width / 2), Math.round(this.pfeet.y + speedY + this.pfeet.height / 2));
             color += this.wallsBmd.getPixel32(this.pfeet.x + speedX - this.pfeet.width / 2, this.pfeet.y + speedY + this.pfeet.height / 2);
             color += this.wallsBmd.getPixel32(this.pfeet.x + speedX - this.pfeet.width / 2, this.pfeet.y + speedY - this.pfeet.height / 2)
             color += this.wallsBmd.getPixel32(this.pfeet.x + speedX + this.pfeet.width / 2, this.pfeet.y + speedY - this.pfeet.height / 2)
-            if (color === 0x000000) {
+            if (color == 0) {
                 this.pfeet.x += speedX;
                 this.pfeet.y += speedY;
             }
